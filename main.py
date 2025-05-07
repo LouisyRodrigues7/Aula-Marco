4b2aeb9fdf7cbf61046c6a2092dd6eef02261a21
LouisyRodrigues7 <louisy.marcelle@gmail.com>
2025-05-07 16:39:35 -0300

    Usando POST + Insomnia

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
+from flask import request, jsonify
 
 class Base(DeclarativeBase):
-  pass
+    pass
 
 db = SQLAlchemy(model_class=Base)
 
@@ -29,6 +30,16 @@ class Book(db.Model):
 # Criação da tabela no banco
 with app.app_context():
     db.create_all()
+    
+# POST
+@app.route("/books",methods=["POST"])
+def add_book():
+    data=request.get_json()
+    new_book = Book(author=data["author"], genre=data["genre"], year=data["year"],title=data["title"]
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({"message": "Livro adicionado com sucesso!"}), 201
 
 @app.route("/")
 def hello_world():
@@ -37,3 +48,10 @@ def hello_world():
 @app.route("/teste")
 def rota_teste():
     return "<p>Rota, teste!</p>"
+
+
+# {
+	#"author": "Tester",
+	#"genre": "Livro de Testes",
+	#"year": "2025",
+	#"title": "Uma Introdução ao Teste" }
